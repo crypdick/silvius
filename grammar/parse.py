f8206bed2dd9f727c2ac3eede076a0fc8ee68fe7
Richard Decal <richard.decal@ncf.edu>
2018-06-14 10:44:36 -0700

    dash as alt alias to minus

--- grammar/parse.py
+++ grammar/parse.py
@@ -175,6 +175,7 @@ class CoreParser(GenericParser):
             character ::= late
             character ::= rate
             character ::= minus
+            character ::= dash
             character ::= underscore
             character ::= plus
             character ::= backslash
@@ -206,6 +207,7 @@ class CoreParser(GenericParser):
             'late': 'parenleft',
             'rate': 'parenright',
             'minus': 'minus',
+            'dash': 'minus',
             'underscore': 'underscore',
             'plus': 'plus',
             'backslash': 'backslash',
